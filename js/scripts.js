360292b74043fd83a75a20bc7b4b4076ed20216a
Zarkwolff <diegogp11@gmail.com>
2023-07-03 11:14:03 -0300

    criando a funcionalidade do menu mobile

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+//Seleção de elementos
+const menuBtn = document.querySelector("#menu");
+const closeMenuBtn = document.querySelector("#close-menu");
+const menu = document.querySelector("#mobile-navbar");
+
+//Funções
+
+//Eventos
+[menuBtn, closeMenuBtn].forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        menu.classList.toggle("menu-active");
+    });
+});
