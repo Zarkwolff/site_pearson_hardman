834825988b3b8365460457870da1f4978dc7bbe0
Zarkwolff <diegogp11@gmail.com>
2023-07-03 12:10:31 -0300

    implementando o menu de ancoragem

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,38 @@ const menuBtn = document.querySelector("#menu");
 const closeMenuBtn = document.querySelector("#close-menu");
 const menu = document.querySelector("#mobile-navbar");
 
+const desktopLinks = document.querySelectorAll("#navbar a");
+const mobileLinks = document.querySelectorAll("#mobile-navbar a");
+const allLinks = [...desktopLinks, ...mobileLinks];
+
+console.log(allLinks);
+
 //Funções
+function smoothScroll(e){
+    e.preventDefault();
+
+    const href = this.getAttribute("href");
+    const offsetTop = document.querySelector(href).offsetTop;
+
+    scroll({
+        top: offsetTop,
+        behavior: "smooth",
+    });
+
+    setTimeout(() => {
+        if(menu.classList.contains("menu-active")){
+            menu.classList.remove("menu-active");
+        }
+    }, 500);
+}
 
 //Eventos
 [menuBtn, closeMenuBtn].forEach((btn) => {
     btn.addEventListener("click", (e) => {
         menu.classList.toggle("menu-active");
     });
+});
+
+allLinks.forEach((link) => {
+    link.addEventListener("click", smoothScroll);
 });
